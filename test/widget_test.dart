097e8b43fdb378d13f29be18f6a5b3c68c2efd8d
miyazakiari <skirnir11981198@gmail.com>
2021-09-20 01:06:20 +0900

    サムネ画像アップロードできない

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:movie_practice/main.dart';
-import 'package:movie_practice/movie.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Movie());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
